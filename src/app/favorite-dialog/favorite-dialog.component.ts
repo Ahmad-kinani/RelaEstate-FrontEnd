0a59233e7f71c565214b9c11811dcbc718d765d9
abode <abode@amescom.com>
2024-09-11 13:27:28 +0300

    d

--- src/app/favorite-dialog/favorite-dialog.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-favorite-dialog',
-  templateUrl: './favorite-dialog.component.html',
-  styleUrls: ['./favorite-dialog.component.css'],
-})
-export class FavoriteDialogComponent {}
